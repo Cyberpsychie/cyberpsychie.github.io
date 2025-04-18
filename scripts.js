918d951e0c74e155f259dc9fa368167341134559
Cyberpsychie <Soapie091702@gmail.com>
2025-04-17 21:28:25 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,43 @@
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
 
 function menuDrop() {
   document.getElementById("myDropdown").classList.toggle("show");
   var x = document.getElementById('rect');
   if (x.style.visibility === 'visible') {
     x.style.visibility = 'hidden';
+	window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+	window.removeEventListener('touchmove', preventDefault, wheelOpt);
+	window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
   } else {
     x.style.visibility = 'visible';
+	window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+	window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+	window.addEventListener('keydown', preventDefaultForScrollKeys, false); //keyboard
   }
 }
 
+function menuAni(x) {
+  x.classList.toggle("change");
+}
